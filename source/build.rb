f8d04c4fa5338a4921bd6db4c47d16f88b149afa
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-09-16 15:49:51 +1200

    Source code for visualisation tool, extracted from transform flow.

---
+++ source/build.rb
@@ -0,0 +1,14 @@
+
+add_application 'Transform Flow' do
+	compile_executable 'transform-flow' do
+		def source_files(environment)
+			FileList[root, "TransformFlow-Visualisation/**/*.cpp"]
+		end
+	end
+	
+	copy_files do
+		def source_files(environment)
+			FileList[root + "TransformFlow-Visualisation/Resources", '**/*']
+		end
+	end
+end
