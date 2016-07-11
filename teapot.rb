b14fedcbd94a6f633f69a1f6517f75d926d7139b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-07-11 19:33:30 +1200

    Update teapot build.

--- teapot.rb
+++ teapot.rb
@@ -1,17 +1,25 @@
 
-teapot_version "0.8.0"
+teapot_version "1.0"
 
 define_project "Transform Flow Visualisation" do |project|
 	project.add_author "Samuel Williams"
 	project.license = "MIT License"
 	
-	project.version = "0.1.0"
+	project.version = "0.2.0"
 end
 
 define_target "transform-flow-visualisation" do |target|
 	target.build do |environment|
-		build_directory(package.path, 'source', environment)
+		source_root = target.package.path + 'source'
+		
+		copy headers: source_root.glob('TransformFlow-Visualisation/**/*.{h,hpp}')
+		copy files: source_root.glob('TransformFlow-Visualisation/Resources**/*'), prefix: 'share/TransformFlow-Visualisation'
+		
+		build executable: "TransformFlow", source_files: source_root.glob('TransformFlow-Visualisation/**/*.cpp')
 	end
+	
+	target.depends "Build/Files"
+	target.depends "Build/Clang"
 	
 	target.depends :platform
 	target.depends "Language/C++11"
